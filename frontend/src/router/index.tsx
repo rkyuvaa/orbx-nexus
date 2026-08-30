3b63b7b83b3419c9d57b86ac9e069301e6fe080e
rkyuvaa <rkyuvaa@gmail.com>
2026-08-31 00:41:26 +0530

    Revert "Add Contractor Advance Payment edit functionality"
    This reverts commit 9cfcce5e818ccbc2f7fb24287bfc9c2f585af269.

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -8,8 +8,10 @@ import LedgerPage from "../pages/accounts/LedgerPage";
 import { ProductRegisterPage, ProcessRegisterPage, ProcessGroupsPage, RateRegisterPage, UoMPage } from "../pages/process/ProcessInfoPages";
 import { InwardVoucherPage, OutwardVoucherPage } from "../pages/process-voucher/ProcessVoucherPages";
 import LabourBillPage from "../pages/labour-bill/LabourBillPage";
-import { SalaryVoucherPage } from "../pages/payroll/PayrollPages";
-import ContractorAdvancePayment from "../pages/contractor/ContractorAdvancePayment";
+import {
+  ContractorAdvancePaymentPage, ContractorAdvanceReceiptPage,
+  SalaryVoucherPage
+} from "../pages/payroll/PayrollPages";
 import {
   PaymentVoucherPage, ReceiptVoucherPage, ContraVoucherPage,
   JournalVoucherPage, PurchaseVoucherPage, MiscExpensesPage
@@ -98,8 +100,8 @@ export const router = createBrowserRouter([
       // Contractor Voucher
       { path: "contractor/rates", element: <ContractorPages type="rates" /> },
       { path: "contractor/job-work", element: <ContractorPages type="job-work" /> },
-      { path: "contractor/advance-payment", element: <ContractorAdvancePayment /> },
-      { path: "contractor/advance-receipt", element: <ContractorAdvancePayment /> },
+      { path: "contractor/advance-payment", element: <ContractorAdvancePaymentPage /> },
+      { path: "contractor/advance-receipt", element: <ContractorAdvanceReceiptPage /> },
       { path: "contractor/payment", element: <ContractorPages type="payment" /> },
 
       // Accounts Voucher
